92a24f6f7049bc15b9daaed19507768e538e7980
FreitagDavid <freitagdavid@outlook.com>
2019-01-17 16:16:33 -0500

    migrations/20190117161033_create_recipesingredients: create

---
+++ migrations/20190117161033_create_recipesingredients.js
@@ -0,0 +1,22 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipesingredients', table => {
+        table.increments();
+        table
+            .string('ingredient_id')
+            .unsigned()
+            .references('id')
+            .inTable('ingredients')
+            .notNullable();
+        table
+            .string('recipe_id')
+            .unsigned()
+            .references('id')
+            .inTable('recipes')
+            .notNullable();
+        table.float('quantity').notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('recipesingredients');
+};
